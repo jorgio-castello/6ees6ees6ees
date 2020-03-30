71ee75c6785b3c244f68b1d46847400deb70fd3a
Jorge Castello <jorgio.castello@gmail.com>
2020-03-30 10:13:06 -0700

    Create Grub Class with the following properties:
    Age property set to 0
    Color property set to pink
    Food property set to jelly
    Eat method that returns 'Mmmmmmmmm jelly'

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,10 @@
 class Grub {
+  constructor(age, color, food) {
+    this.age = age || 0
+    this.color = color || 'pink'
+    this.food = food  || 'jelly'
+  }
+  eat() {
+    return 'Mmmmmmmmm jelly'
+  }
 };
