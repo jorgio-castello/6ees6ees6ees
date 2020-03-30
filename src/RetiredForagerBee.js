9c4155a5bcdfd0db416cba9da3ebb0c901cf65e3
Jorge Castello <jorgio.castello@gmail.com>
2020-03-30 10:51:01 -0700

    Create RetiredForagerBee Class- extends ForagerBee

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,11 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor(age = 40, job = 'gamble', canFly = false, color = 'grey') {
+    super(age, job, canFly, color)
+  }
+  forage() {
+    return 'I am too old, let me play cards instead'
+  }
+  gamble(treasure) {
+    this.treasureChest.push(treasure)
+  }
 };
