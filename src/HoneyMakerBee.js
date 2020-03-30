e7b575fbafc7f167370a9c79391f5db59fdffe12
Jorge Castello <jorgio.castello@gmail.com>
2020-03-30 10:22:17 -0700

    Create HoneyMakerBee class that extends Bee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,12 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor(age = 10, color, job = 'make honey') {
+    super(age, job)
+    this.honeyPot = 0
+  }
+  makeHoney() {
+    this.honeyPot++
+  }
+  giveHoney() {
+    this.honeyPot--
+  }
 };
