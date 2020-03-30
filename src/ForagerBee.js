0730896dae932aa1a9c557a220786e7ce7849ae7
Jorge Castello <jorgio.castello@gmail.com>
2020-03-30 10:50:21 -0700

    Create ForagerBee Class that extends Bee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,10 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor(age = 10, job = 'find pollen', canFly = true, color) {
+    super(age, job, color)
+    this.canFly = canFly
+    this.treasureChest = []
+  }
+  forage(treasure) {
+    this.treasureChest.push(treasure)
+  }
 };
